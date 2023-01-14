7f328d3b31d0fd7d60ede3751bb712abd3606652
Anibladze1 <ako.nibladze1@gmail.com>
2023-01-14 18:05:08 +0400

    models

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Manufacturer, Make
 
 # Register your models here.
+admin.site.register(Manufacturer)
+admin.site.register(Make)
--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100)
+    brand_creation_date = models.DateField()
+    picture = models.ImageField(upload_to="author/", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Make(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    engine_vol = models.FloatField(max_length=100)
+    release_date = models.DateField()
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="make")
+
+    def __str__(self):
+        return self.name
